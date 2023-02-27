d61417dbf2ec1c2baa334f06eee4e990aa02b588
Romaric Zounlome <rzounlom@gmail.com>
2023-02-27 09:23:20 -0500

    add Sidebar/SidebarLink components

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,22 @@
+import "@/styles/global.css";
+
+import GlassPane from "@/components/GlassPane";
+
+// import { Inter } from "@next/font/google";
+
+// const inter = Inter({
+//   variable: "--font-inter",
+// });
+
+export default function AuthRootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head />
+      <body className="h-screen w-screen rainbow-mesh p-6">
+        <GlassPane className="w-full h-full flex items-center justify-center">
+          {children}
+        </GlassPane>
+      </body>
+    </html>
+  );
+}
---
+++ src/components/SidebarLink.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Calendar, Grid, Settings, User } from "react-feather";
+
+import Link from "next/link";
+import clsx from "clsx";
+import { usePathname } from "next/navigation";
+
+const icons = { Settings, User, Grid, Calendar };
+
+const SidebarLink = ({ link }) => {
+  const pathname = usePathname();
+  let isActive = false;
+
+  if (pathname === link.link) {
+    isActive = true;
+  }
+
+  const Icon = icons[link.icon];
+  return (
+    <Link href={link.link} className="w-full flex justify-center items-center">
+      <Icon
+        size={40}
+        className={clsx(
+          "stroke-gray-400 hover:stroke-violet-600 transition duration-200 ease-in-out",
+          isActive && "stroke-violet-600"
+        )}
+      />
+    </Link>
+  );
+};
+
+export default SidebarLink;
